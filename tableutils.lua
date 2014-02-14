88269efa152ae8bea851afcb45fa726d82aa7a17
Jeduan Cornejo <jeduan@gmail.com>
2014-02-14 15:04:54 -0600

    initial commit

---
+++ tableutils.lua
@@ -0,0 +1,30 @@
+local log = require 'vendor.log.log'
+
+local M = {}
+
+function M.shuffle(t)
+	math.randomseed(os.time())
+	assert(t, "table.shuffle() expected a table, got nil")
+	local iterations = #t
+	local j
+	for i = iterations, 2, -1 do
+		j = math.random(i)
+		t[i], t[j] = t[j], t[i]
+	end
+	return t
+end
+
+function M.extend(dest, source)
+	for k,v in pairs(source) do
+		destination[k] = v
+	end
+	return destination
+end
+
+function M.infect()
+	for _, fn in {'shuffle', 'extend'} do
+		table[fn] = M[fn]
+	end
+end
+
+return M
